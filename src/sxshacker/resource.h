70cbcf7153026dd3be52bf4d1e59c6caa724d3ca
tokillms <hacker931@163.com>
2014-01-02 12:17:08 +0800

    init project with basic MainDlg and AboutDlg

---
+++ src/sxshacker/resource.h
@@ -0,0 +1,26 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by sxshacker.rc
+//
+#define IDD_SXSHACKER_DIALOG            102
+#define IDD_ABOUTBOX                    103
+#define IDI_SXSHACKER                   107
+#define IDR_MAINFRAME                   128
+#define IDD_DIALOG_MAIN                 129
+#define IDS_ABOUTBOX                    130
+#ifndef IDC_STATIC
+#define IDC_STATIC                      -1
+#endif  // IDC_STATIC
+#define IDM_ABOUTBOX                    0x0010
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32772
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
